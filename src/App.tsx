418e97b40b0f8f71a64b34b14f6def8a60d0a131
Alexandra Staszewski <alexandra_sta@yahoo.de>
2024-12-18 13:21:32 +0100

    Main Setup and Navigation

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/scenes/navbar/index.tsx";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  return (
+  <div className="app bg-gray-20">
+    <Navbar 
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+    />
+  </div>
+  );
+}
+
+export default App;
