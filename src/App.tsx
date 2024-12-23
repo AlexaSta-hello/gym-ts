b06c1fbd2c31b058c90b349acbbdfb99bf625a23
Alexandra Staszewski <alexandra_sta@yahoo.de>
2024-12-23 13:36:56 +0100

    Home and Benefits Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import Navbar from "@/scenes/navbar/index.tsx";
-import { useState } from "react";
+import Home from "./scenes/home";
+import Benefits from "@/scenes/benefits";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true); /* navbar farblich unterlegen, beim scrollen */
+
+  useEffect (() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []);
+
   return (
   <div className="app bg-gray-20">
-    <Navbar 
+    <Navbar
+      isTopOfPage={isTopOfPage} 
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
     />
+
+    <Home setSelectedPage={setSelectedPage}/>
+    <Benefits setSelectedPage={setSelectedPage}/>
+    
   </div>
   );
 }
